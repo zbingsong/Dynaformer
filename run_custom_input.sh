8e2b20a807fa250b2192e457e0a09eb0531f1271
Bingsong Zeng <bingsong.zeng@uth.tmc.edu>
2025-10-14 13:37:20 -0500

    removed legacy scripts

--- run_custom_input.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
-bash Dynaformer/examples/evaluate/evaluate.sh "" "custom:path=$SCRIPT_DIR/example_data/example.pkl" "" _custom
